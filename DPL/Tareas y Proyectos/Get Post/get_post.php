08a85c0f22567522652169fa2c6b9bdb0517902b
Cesardrom <cesardguezr20@gmail.com>
2024-09-24 15:04:31 +0100

    Subimos archivos de php

---
+++ DPL/Tareas y Proyectos/Get Post/get_post.php
@@ -0,0 +1,10 @@
+<?php
+    echo  "-pre";
+    print_r $_GET;
+    echo "<br>";
+    print_r( $_GET['usuario']);
+
+    echo  "-pre";
+    print_r $_POST;
+    echo "<br>";
+    print_r( $_POST['usuario']);
