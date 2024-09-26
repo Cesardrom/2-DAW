75981d04768219829f528fe26037e48458704fb6
Cesardrom <145054495+Cesardrom@users.noreply.github.com>
2024-09-26 18:16:14 +0100

    Subimos lo hecho de pokemon

---
+++ DEW/Tareas y Proyectos/1º Trimestre/Pokemon/app.js
@@ -0,0 +1,31 @@
+const button = document.querySelector('button');
+
+function mostrarPokemon(){
+    document.querySelector('.cargandoDatos').style.visibility = "visible";
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", (e) =>{
+        if (e.target.readyState === 4){
+            const datos = JSON.parse(e.target.responseText);
+            console.log(datos)
+        }
+    })
+    request.open("GET", "https://pokeapi.co/api/v2/pokemon/")
+    request.send();
+};
+
+button.addEventListener('click', () => {
+    document.querySelectorAll("#filtro").forEach( (e) =>{
+        e.style.visibility = "visible";
+    });
+
+    document.querySelectorAll('#btn_lista_deseo').forEach((e) => {
+        e.style.visibility = "visible";
+    });
+
+    let listaPokemon  = document.querySelector('listaPokemon');
+    listaPokemon.style.visibility = "visible";
+    mostrarPokemon();
+});
+
