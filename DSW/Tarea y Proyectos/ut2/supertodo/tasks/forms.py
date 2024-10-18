37fa9d23a9920aeab04ba70d59600473ea8f41e1
Cesardrom <cesardguezr20@gmail.com>
2024-10-18 20:27:38 +0100

    subimos dsw

---
+++ DSW/Tarea y Proyectos/ut2/supertodo/tasks/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+from .models import Task
+
+
+class AddTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('name', 'description', 'done', 'complete_before')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+
+class EditTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('name', 'description', 'done', 'complete_before')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- DSW/Tarea y Proyectos/ut2/supertodo/tasks/views.py
+++ DSW/Tarea y Proyectos/ut2/supertodo/tasks/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import redirect, render
+from django.utils.text import slugify
 
 from tasks.models import Task
+
+from .forms import AddTaskForm, EditTaskForm
 
 
 # Create your views here.
@@ -16,15 +19,35 @@ def task_list(request):
 
 
 def done_list(request):
-    return HttpResponse(request)
+    total_tasks = Task.objects.filter(done=True)
+    return render(
+        request,
+        'tasks/task_list.html',
+        {
+            'total_tasks': total_tasks,
+        },
+    )
 
 
 def pending_list(request):
-    return HttpResponse(request)
+    total_tasks = Task.objects.filter(done=False)
+    return render(
+        request,
+        'tasks/task_list.html',
+        {'total_tasks': total_tasks},
+    )
 
 
 def add_task(request):
-    return HttpResponse(request)
+    if request.method == 'POST':
+        if (form := AddTaskForm(request.POST)).is_valid():
+            task = form.save(commit=False)
+            task.slug = slugify(task.name)
+            task.save()
+            return redirect('tasks:task-list')
+    else:
+        form = AddTaskForm()
+    return render(request, 'tasks/modified/add_task.html', dict(form=form))
 
 
 def task_detail(request, slug: str):
@@ -32,13 +55,29 @@ def task_detail(request, slug: str):
     return render(request, 'tasks/task/detail.html', dict(task=task))
 
 
-def toggle_task(request):
-    return HttpResponse(request)
+def toggle_task(request, slug: str):
+    task = Task.objects.filter(slug=slug)
+    if task.done:
+        task.update(done=False)
+    else:
+        task.update(done=True)
+    return render(request, 'tasks/task_list.html', {'task': task})
 
 
-def edit_task(request):
-    return HttpResponse(request)
+def edit_task(request, slug: str):
+    task = Task.objects.get(slug=slug)
+    if request.method == 'POST':
+        if (form := EditTaskForm(request.POST, instance=task)).is_valid():
+            task = form.save(commit=False)
+            task.slug = slugify(task.name)
+            task.save()
+            return redirect('tasks:task-list')
+    else:
+        form = EditTaskForm(instance=task)
+    return render(request, 'tasks/modified/edit_task.html', dict(post=task, form=form))
 
 
-def delete_task(request):
-    return HttpResponse(request)
+def delete_task(request, slug: str):
+    task = Task.objects.get(slug=slug)
+    task.delete()
+    return render(request, 'tasks/task_list.html')
